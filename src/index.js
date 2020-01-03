1779351fe91f2f3886fc7a712e0e2e894994d18b
SalCy <46075019+salwacynthia@users.noreply.github.com>
2020-01-03 23:41:28 +0100

    A React App to learn Capital City of different countries

--- src/index.js
+++ src/index.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Quiz extends Component {
+    state = {
+        questionBank:[],  // 5 questions will be stored when we will call the API
+        score: 0,
+        responses : 0
+    };
+    getQuestion = () =>{
+         quizService().then(question => {  //calling the quizservice API
+             this.setState({   // updating the state variable
+                 questionBank:question
+             });
+         });
+   };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    computeAnswer = (answer, correctAnswer) => {
+       if (answer === correctAnswer) {
+           this.setState ({
+               score: this.state.score + 1
+           });
+       }
+       this.setState ({
+           responses : this.state.responses < 5 ? this.state.responses + 1 : 5 
+       })
+    };
+    playAgain = () => {
+        this.getQuestion();
+        this.setState({
+          score: 0,
+          responses:0
+        });
+    }
+   componentDidMount (){
+       this.getQuestion();
+   }
+   render (){
+       return (
+           <div className = "container">
+               <div className = "title"> Quiz  </div>
+               {this.state.questionBank.length > 0 &&  // checking conditions to show the set of questions
+               this.state.responses < 5 && 
+               this.state.questionBank.map(({question, answers,
+               correct, questionId})=> (
+                <QuestionBox        
+                 question = {question} 
+                 options = {answers} 
+                 key = {questionId}
+                 selected = {answer => this.computeAnswer(answer, correct)}
+                />
+            )
+        )}
+        {this.state.responses === 5 ? (<Result score = {this.state.score} playAgain = {this.playAgain}/> ) : null} 
+     </div>
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+ReactDOM.render (<Quiz/>, document.getElementById("root"));
---
+++ src/quizService/index.js
@@ -0,0 +1,372 @@
+const qBank = [
+  {
+    question:
+      "what is the capital of Germany? ",
+    answers: ["Bonn", "Munich", "Berlin", "Stuttgurt"],
+    correct: "Berlin",
+    questionId: "099099"
+  },
+  {
+    question:
+      'what is the capital of Bangladesh?',
+    answers: ["Madrid", "Noakhali", "Dhaka", "New Delhi"],
+    correct: "Dhaka",
+    questionId: "183452"
+  },
+  {
+    question:
+      "what is the capital of Japan?",
+    answers: ["Barcelona", "Tokyo", "Fiji", "Schenchu"],
+    correct: "Tokyo",
+    questionId: "267908"
+  },
+  {
+    question: "what is the capital of the United Kingdom?",
+    answers: [
+      "London",
+      "Washington DC",
+      "Newyork",
+      "Ireland"
+    ],
+    correct: "London",
+    questionId: "333247"
+  },
+  {
+    question: "what is the capital of Hungary?",
+    answers: ["Berlin", "Prague", "Budapest", "Paris"],
+    correct: "Budapest",
+    questionId: "496293"
+  },
+  {
+    question:
+      "what is the capital of Sweden?",
+    answers: [
+      "Copenhegen",
+      "Stockholm",
+      "Tallinn",
+      "Singapore City"
+    ],
+    correct: "Stockholm",
+    questionId: "588909"
+  },
+  {
+    question:
+      'what is the capital of Denmark?',
+    answers: ["Dhaka", "Copenhegen", "Beirut", "Bercelona"],
+    correct: "Copenhegen",
+    questionId: "648452"
+  },
+  {
+    question: "what is the capital of Russia?",
+    answers: ["Moscow", "St. Peters", "Leviticus", "Nairobi"],
+    correct: "Moscow",
+    questionId: "786649"
+  },
+  {
+    question:
+      "what is the capital of Norway?",
+    answers: ["Moscow", "Stockholm", "Copenhegen", "Oslo"],
+    correct: "Oslo",
+    questionId: "839754"
+  },
+  {
+    question:
+      "what is the capital of France?",
+    answers: [
+      "Oslo",
+      "Paris",
+      "Prague",
+      "Rubiks"
+    ],
+    correct: "Paris",
+    questionId: "98390"
+  },
+  {
+    question: "what is the capital of Switzerland?",
+    answers: ["Geneva", "Zurich", "Bern", "Bonn"],
+    correct: "Bern",
+    questionId: "1071006"
+  },
+  {
+    question: "what is the capital of Canada?",
+    answers: ["Ottawa", "Tokyo", "Geneva", "Toronto"],
+    correct: "Ottawa",
+    questionId: "1174154"
+  },
+  {
+    question: "what is the capital of Brazil?",
+    answers: ["St.Paolo", "Arizona", "New Mexico", "Brasilia"],
+    correct: "Brasilia",
+    questionId: "1226535"
+  },
+  {
+    question: "what is the capital of SouthAfrica?",
+    answers: ["Cape Town", "Brasilia", "Peru", "Seol"],
+    correct: "Cape Town",
+    questionId: "1310938"
+  },
+  {
+    question: "what is the capital of Spain?",
+    answers: ["Granada", "Mallorca", "Barcelona", "Madrid"],
+    correct: "Madrid",
+    questionId: "1436873"
+  },
+  {
+    question: "what is the capital of India?",
+    answers: ["New Delhi", "Dhaka", "Bombay", "Lukhnow"],
+    correct: "New Delhi",
+    questionId: "1515110"
+  },
+  {
+    question: "what is the capital of Nepal?",
+    answers: ["Thimpu", "Kathmandu", "Pokhara", "Thiup"],
+    correct: "Kathmandu",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "what is the capital of Bhutan? ",
+    answers: ["Bhutan", "Thimpu", "Kathmandu", "Pokhra"],
+    correct: "Thimpu",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "what is the capital of Srilanka?",
+    answers: ["Colombo", "Thimpu", "Kathmandu", "Turkey"],
+    correct: "Colombo",
+    questionId: "1822532"
+  },
+  {
+    question: "what is the capital of Turkey?",
+    answers: [
+      "Ankara",
+      "Istanbul",
+      "Abu dhabi",
+      "Beirut"
+    ],
+    correct: "Ankara",
+    questionId: "195075"
+  },
+  {
+    question: 'what is the capital of Fiji?',
+    answers: ["Fiji", "Madrid", "Suva", "Ankara"],
+    correct: "Suva",
+    questionId: "2019778"
+  },
+  {
+    question: "what is the capital of Finland?",
+    answers: ["Helsinki", "Paris", "Berlin", "London"],
+    correct: "Helsinki",
+    questionId: "2134343"
+  },
+  {
+    question: "what is the capital of Belgium?",
+    answers: [
+      "Berlin",
+      "Brussels",
+      "Amsterdam",
+      "Prague"
+    ],
+    correct: "Brussels",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "what is the capital of Australia?",
+    answers: ["Canberra", "Melborne", "Sydney", "Fiji"],
+    correct: "Canberra",
+    questionId: "2379831"
+  },
+  {
+    question: "what is the capital of Croatia?",
+    answers: ["Paris", "Zagreb", "Berlin", "Madrid"],
+    correct: "Zagreb",
+    questionId: "2426418"
+  },
+  {
+    question: "what is the capital of Estonia?",
+    answers: ["Zagreb", "Thump", "Tallinn", "Sydney"],
+    correct: "Tallinn",
+    questionId: "2510086"
+  },
+  {
+    question: "what is the capital of Iran?",
+    answers: [
+      "Tehran",
+      "Bahrain",
+      "Estonia",
+      "Baghdad"
+    ],
+    correct: "Tehran",
+    questionId: "2685745"
+  },
+  { 
+    question:
+      "what is the capital of Iraq?",
+    answers: ["Baghdad", "Tehran", "Beirut", "Abu Dhabi"],
+    correct: "Baghdad",
+    questionId: "2796884"
+  },
+  {
+    question: "what is the capital of Italy?",
+    answers: ["Vatican", "Rome", "Pompey", "Venice"],
+    correct: "Rome",
+    questionId: "2838900"
+  },
+  {
+    question: "what is the capital of Kenya?",
+    answers: ["Nairobi", "Bonn", " Écharpe", "Casque"],
+    correct: "Nairobi",
+    questionId: "298060"
+  },
+  {
+    question: "what is the capital of Lebanon?",
+    answers: ["Beirut", "Tehran", "Baghdad", "Nairobi"],
+    correct: "Beirut",
+    questionId: "3096579"
+  },
+  {
+    question: "what is the capital of Libya?",
+    answers: [
+      "Tripoli",
+      "Ankara",
+      "Tehran",
+      "Malta"
+    ],
+    correct: "Tripoli",
+    questionId: "3182461"
+  },
+  {
+    question: "what is the capital of Malta?",
+    answers: [
+      "Valletta",
+      "Malta",
+      "Venice",
+      "Glasgow"
+    ],
+    correct: "Valletta",
+    questionId: "3239112"
+  },
+  {
+    question: "what is the capital of Mexico?",
+    answers: ["Mexico City", "Canberra", "New York", "Peru"],
+    correct: "Mexico City",
+    questionId: "3318503"
+  },
+  {
+    question:"what is the capital of the Netherlands?",
+    answers: ["Amsterdam", "Bern", "Bonn", "Brussels"],
+    correct: "Amsterdam",
+    questionId: "3410327"
+  },
+  {
+    question: "what is the capital of Poland?",
+    answers: ["Polska", "Warsaw", "Brussels", "Póland"],
+    correct: "Warsaw",
+    questionId: "3542596"
+  },
+  {
+    question: "what is the capital of Oman? ",
+    answers: ["Beirut", "Abu Dhabi", "Muscat", "Riyadh"],
+    correct: "Muscat",
+    questionId: "3667517"
+  },
+  {
+    question: "what is the capital of Panama?",
+    answers: [
+      "Panama City",
+      "Kiwi",
+      "Honololo",
+      "Canberra"
+    ],
+    correct: "Panama City",
+    questionId: "3791672"
+  },
+  {
+    question: "what is the capital of Saudi Arabia?",
+    answers: ["Beirut", "Medina", "Riyadh", "Tallinn"],
+    correct: "Riyadh",
+    questionId: "3893585"
+  },
+  {
+    question: "what is the capital of Syria?",
+    answers: ["Damascus", "Doha", "Beirut", "Oman"],
+    correct: "Damascus",
+    questionId: "3913430"
+  },
+  {
+    question: "what is the capital of Thailand? ",
+    answers: ["Thai", "Bankok", "Singapore", "Helsinki"],
+    correct: "Bankok",
+    questionId: "4049121"
+  },
+  {
+    question:"what is the capital of United Arab Emirates? ",
+    answers: ["Abu Dhabi", "UAE", "Oman", "Munchkin"],
+    correct: "Abu Dhabi",
+    questionId: "4150746"
+  },
+  {
+    question: "what is the capital of Tunisia?",
+    answers: ["Tuniska", "Tunis", "Tallinn", "Belgium"],
+    correct: "Tunis",
+    questionId: "4235063"
+  },
+  {
+    question: "what is the capital of the United States?",
+    answers: ["Washington DC", "New York", "Mexico", "Arizoana"],
+    correct: "Washington DC",
+    questionId: "4321002"
+  },
+  {
+    question: "what is the capital of Vatican City?",
+    answers: [
+      "Rome",
+      "Venice",
+      "Prague",
+      "Vatican City",
+    ],
+    correct: "Vatican City",
+    questionId: "4442286"
+  },
+  {
+    question: "what is the capital of Vietnam?",
+    answers: [
+      "Fiji",
+      "Nairobi",
+      "Hanoi",
+      "Thimpu"
+    ],
+    correct: "Hanoi",
+    questionId: "4564430"
+  },
+  {
+    question: "what is the capital of Zimbabwe?",
+    answers: ["Holland", "Sega", "Harare", "Kiwi"],
+    correct: "Harare",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "what is the capital of China?",
+    answers: ["Beijing", "Schenchau", "Korea", "United States of America"],
+    correct: "Beijing",
+    questionId: "4795960"
+  },
+  {
+    question: "what is the capital of Austria?",
+    answers: ["Innbruck", "Salsburg", "Vienna", "Achen"],
+    correct: "Vienna",
+    questionId: "4811162"
+  },
+  {
+    question: "what is the capital of Afganistan?",
+    answers: ["Kabul", "Uzbekistan", "Tehran", "Beirut"],
+    correct: "Kabul",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
